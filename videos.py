d82de9d52cf8db84bd65591eea0f04a070297c2d
harlleyaugusto <harlleyaugusto@gmail.com>
2019-10-09 18:08:29 +0200

    User profile v1

--- videos.py
+++ videos.py
@@ -31,33 +31,30 @@ if __name__ == '__main__':
     data = pd.read_csv("data/videos.csv")
     data = data[data.relevant]
 
+    #Completeness calculation
+    comp = []
+    for index, row in data.iterrows():
+        print(str(index) + "/" + str (data.__len__()))
+        comp.append(completeness(row))
+
     descriptions = data['video_description'].apply(remove_ponctuation)
     readability_fre = descriptions.apply(lambda text: textstat.flesch_reading_ease(text) if (text is not None) else text)
 
     l_d = descriptions.apply(length_description)
 
-
-
+    data['completeness'] = list(comp)
     data['flesch_reading_ease'] = list(readability_fre)
     data['description_length'] = list(l_d)
     data['description_level'] = l_d.apply(description_classifier)
 
-    #composer_uploaders = data.groupby(['video_uploader_id', 'composer_name']).count()
-    #s_sum =  data.groupby('video_uploader_id').sum()
+    uploaders_group = data[data.description_level].groupby(['video_uploader_id', 'composer_name'])
 
-    # Completeness calculation
-    # comp = []
-    # for index, row in data.iterrows():
-    #     print(str(index) + "/" + str (data.__len__()))
-    #     comp.append(completeness(row))
+    uploaders_count = uploaders_group[['video_url', 'video_likes', 'video_dislikes', 'video_comments']].count()
 
-    # u = list(uploaders.index)
-    # up = list(uploaders.iloc[:,0])
-    # l = list(s_sum.iloc[:,1])
-    # d = list(s_sum.iloc[:,2])
-    # c = list(s_sum.iloc[:,6])
-    #
-    # df = {'user_id': u, 'user_count': up, 'user_likes': l, 'user_dislike': d, 'user_comments': c}
-    #
-    # df = pd.DataFrame(df)
-    # df.to_csv("data/s_videos.csv", index = False)
+    uploaders_count = uploaders_count.rename(columns={"video_url": "video_count"})
+
+    uploaders_avg = uploaders_group[['completeness','flesch_reading_ease', 'description_length']].mean()
+
+    user_profile = pd.merge(uploaders_count, uploaders_avg, left_index=True, right_index=True)
+
+    user_profile.to_csv("data/user_profile.csv")
