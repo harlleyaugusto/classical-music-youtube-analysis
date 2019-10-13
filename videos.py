e40bfec36c39f35f34e10e23e7b721bd4727c104
harlleyaugusto <harlleyaugusto@gmail.com>
2019-10-13 11:49:07 +0200

    Off to Portugal

--- videos.py
+++ videos.py
@@ -6,23 +6,29 @@ import itertools
 from itertools import cycle
 from itertools import repeat
 from statistics import mean
+from nltk.tokenize import word_tokenize
 
 
 def completeness(video):
     if video is not None:
-         return video[video.notnull()].__len__() / video.keys().__len__()
+        total = video[video.notnull()].__len__()
+        if video['video_tags'] is not None or video['video_tags'].__len__() == 0:
+            total = total - 1
+        if video['video_categories'] is not None or video['video_categories'].__len__() == 0:
+            total = total - 1
+        return total / video.keys().__len__()
     else:
          return 0
 
 def remove_ponctuation(text):
     if text is not None and isinstance(text, str):
-        return text.translate(str.maketrans('', '', string.punctuation))
+        return text.translate(str.maketrans('', '', string.punctuation)).lower()
     else:
         None
 
 def length_description(text):
     if text is not None and isinstance(text, str):
-        return text.__len__()
+        return word_tokenize(text).__len__()
     else:
         return 0
 
@@ -37,29 +43,39 @@ if __name__ == '__main__':
     data = pd.read_csv("data/videos.csv")
     data = data[data.relevant]
 
-    #Completeness calculation
-    comp = []
-    for index, row in data.iterrows():
-        print(str(index) + "/" + str (data.__len__()))
-        comp.append(completeness(row))
+    # Description cleanning
+    descriptions = data['video_description'].apply(remove_ponctuation)
+
+    #description classifier
+    data['description_level'] = descriptions.apply(length_description).apply(description_classifier)
+
+    # Selecting only description with more than 2 words
+    data = data[data.description_level]
 
     descriptions = data['video_description'].apply(remove_ponctuation)
+
+    #Completeness calculation
+    comp = [completeness(row) for index, row in data.iterrows()]
+
+    #Readability
     readability_fre = descriptions.apply(lambda text: textstat.flesch_reading_ease(text) if (text is not None) else text)
 
+    #Description length
     l_d = descriptions.apply(length_description)
 
     data['completeness'] = list(comp)
     data['flesch_reading_ease'] = list(readability_fre)
     data['description_length'] = list(l_d)
-    data['description_level'] = l_d.apply(description_classifier)
 
-    #Mean consine
-    t = []
+
+    #Consine beteween each video's tags and video's composer
+    consine_lst = []
     for index, row in data.iterrows():
         l = eval(row.video_tags)
-        t.append(list(map(textdistance.cosine, l, list(repeat(row.composer_name, l.__len__())))))
+        consine_lst.append(list(map(textdistance.cosine, l, list(repeat(row.composer_name, l.__len__())))))
 
-    mean_cosine = [mean(sorted(i, reverse=True)[0:3]) if (i.__len__() > 0) else 0 for i in t]
+    #Mean of the three greater similarities beteween composer and all tags
+    mean_cosine = [mean(sorted(i, reverse=True)[0:3]) if (i.__len__() > 0) else 0 for i in consine_lst]
 
     data['mean_consine'] = mean_cosine
 
@@ -69,11 +85,9 @@ if __name__ == '__main__':
 
     uploaders_count = uploaders_count.rename(columns={"video_url": "video_count"})
 
-    uploaders_avg = uploaders_group[['completeness','flesch_reading_ease', 'description_length']].mean()
+    uploaders_avg = uploaders_group[['completeness','flesch_reading_ease', 'description_length', 'mean_consine']].mean()
 
     user_profile = pd.merge(uploaders_count, uploaders_avg, left_index=True, right_index=True)
-
-
 
     user_profile.to_csv("data/user_profile.csv")
 
